364943b655cfd73126c72aaafe942f5daed7de12
AY2018 <100161456+AY2018@users.noreply.github.com>
2023-05-10 14:57:02 +0200

    change projects

--- script.js
+++ script.js
@@ -1,16 +1,82 @@
-function updateSupprimerButton() {
-  var checkboxes = document.getElementsByName('supprimer[]');
-  var deleteButton = document.getElementById('BtnSupprimer');
-  var checked = false;
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checked = true;
-      break;
+/* Chart */
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Développer', 'Supprimer', 'Entreprendre', 'Concevoir', 'Communiquer'],
+    datasets: [{
+      data: [1, 2, 3, 2, 1], // Replace with data from your database
+      backgroundColor: '#1eecb7',
+      borderWidth: 0.5, 
+      color: "#fff"
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        min: 0,
+        max: 3
+      }
+    },
+    plugins: {
+    legend: {
+      display: false
+    },
+    title: {
+      display: false
+    },
+    tooltip: {
+      mode: 'index'
+    }
+
     }
   }
-  if (checked) {
-    deleteButton.style.display = "inline-block";
-  } else {
-    deleteButton.style.display = "none";
-  }
+});
+
+Chart.defaults.global.defaultFontColor = "#fff";
+
+/* Project Galleries */
+
+let galleryList = document.getElementById("galleryList");
+let galleryIcons = document.getElementById("galleryIcons");
+
+function listAppear() {
+  let galleryList = document.getElementById("galleryList");
+let galleryIcons = document.getElementById("galleryIcons");
+  galleryList.style.display = "flex";
+  galleryIcons.style.display = "none";
+  
+}
+
+function iconsAppear() {
+  let galleryList = document.getElementById("galleryList");
+let galleryIcons = document.getElementById("galleryIcons");
+  galleryList.style.display = "none";
+  galleryIcons.style.display = "flex";
+}
+
+/* Text area */
+
+const textarea = document.querySelector('#description');
+
+textarea.addEventListener('click', function() {
+  this.selectionStart = 0;
+});
+
+
+/* Traces */
+
+
+function closeTraces(){
+  let trace = document.getElementById('tracesShowcase');
+  trace.classList.add('traceDisapear');
+  trace.classList.remove('traceAppear');
+}
+
+function openTraces(){
+  let trace = document.getElementById('tracesShowcase');
+  trace.classList.add('traceAppear');
+  trace.classList.remove('traceDisapear');
 }
